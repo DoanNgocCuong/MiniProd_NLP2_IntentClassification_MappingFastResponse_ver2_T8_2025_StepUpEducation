8bb7e5942bd3aa6c875c99c4fc4f4fbca417f401
Doan Ngoc Cuong <cuongmkmtpgoldfinch@gmail.com>
2025-08-04 16:13:05 +0700

    update config chuẩn name: --model, --api-key

--- vllm.sh
+++ vllm.sh
@@ -1,6 +1,6 @@
 docker run --runtime nvidia --gpus all \
     -v ~/.cache/huggingface:/root/.cache/huggingface \
-    -p 9090:8000 \
+    -p 30005:8000 \
     --ipc=host \
-    vllm/vllm-openai:latest \
-    vllm --model Qwen/Qwen2.5-1.5B-Instruct --api-keys hoailb-vllm
+    vllm/vllm-openai:v0.4.2 \
+    --model Qwen/Qwen2.5-1.5B-Instruct --api-key hoailb-vllm 
